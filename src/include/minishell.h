16edaf0b2dd702fa4d26e62c492afd726be37ec2
Joseph LANZA <jlanza@student.42.fr>
2023-02-28 19:11:12 +0100

    add pwd, begin of cd, CTRL C, CTRL D, CTRL backslash

--- src/include/minishell.h
+++ src/include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jlanza <jlanza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 15:07:12 by jlanza            #+#    #+#             */
-/*   Updated: 2023/02/27 18:48:35 by jlanza           ###   ########.fr       */
+/*   Updated: 2023/02/28 16:53:00 by jlanza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 # define MINISHELL_H
 
 # include "../../libft/src/libft.h"
-# include "stdio.h"
+# include <stdio.h>
+# include <stdlib.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <signal.h>
 
 # define VERT "\033[0;32m"
 # define ROUGE "\033[0;31m"
@@ -24,6 +26,9 @@
 # define GRAS_START "\e[1m"
 # define GRAS_END "\e[0m"
 # define RESET_COLOR "\033[0m"
+
+# define _XOPEN_SOURCE 700
+# define _GNU_SOURCE
 
 void	print_minishell_title(void);
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jlanza <jlanza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 18:32:12 by jlanza            #+#    #+#             */
-/*   Updated: 2023/02/27 19:01:12 by jlanza           ###   ########.fr       */
+/*   Updated: 2023/02/28 19:03:57 by jlanza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,75 @@
 
 int	g_return_value = 0;
 
+void	handle_sigint(int sig)
+{
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_redisplay();
+	(void)sig;
+}
+
+void	handle_sigquit(int sig)
+{
+	ft_printf("\33[2K\r");
+	rl_on_new_line();
+	rl_redisplay();
+	(void)sig;
+}
+
+void	pwd(void)
+{
+	char	*working_directory;
+
+	working_directory = getcwd(NULL, 0);
+	ft_printf("%s\n", working_directory);
+	free(working_directory);
+}
+
+void	cd(char *path)
+{
+	int		i;
+	char	*new_path;
+
+	i = 0;
+	if (path[0] != '\0')
+	{
+		while (path[i] == ' ')
+			i++;
+		if (chdir(path) == -1)
+		{
+			new_path = ft_strjoin("./", &path[i]);
+			chdir(new_path);
+			free(new_path);
+		}
+	}
+}
+
 int	main(int argc, char *argv[], char *env[])
 {
-	char	do_;
-	char	*cmd_buf;
+	char				do_;
+	char				*cmd_buf;
 
-	do_ = 1;
+	signal(SIGINT, &handle_sigint);
+	signal(SIGQUIT, &handle_sigquit);
 	print_minishell_title();
+	do_ = 1;
 	while (do_)
 	{
 		if (g_return_value == 0)
-			cmd_buf = readline("\033[1;32m➜ \033[0;36m\e[1mminishell$\e[0m \033[0m");
+			cmd_buf = readline("\033[1;32m➜  \033[0;36m\e[1mminishell$\e[0m \033[0m");
 		else
-			cmd_buf = readline("\033[1;31m➜ \033[0;36m\e[1mminishell$\e[0m \033[0m");
-		if (ft_strlen(cmd_buf) > 0)
-			add_history(cmd_buf);
+			cmd_buf = readline("\033[1;31m➜  \033[0;36m\e[1mminishell$\e[0m \033[0m");
+		if (cmd_buf == NULL)
+			return (0);
 		if (!ft_strcmp(cmd_buf, "exit"))
 			do_ = 0;
+		if (ft_strlen(cmd_buf) > 0)
+			add_history(cmd_buf);
+		if (!ft_strncmp(cmd_buf, "pwd ", 4) || !ft_strcmp(cmd_buf, "pwd"))
+			pwd();
+		if (!ft_strncmp(cmd_buf, "cd ", 3))
+			cd(&cmd_buf[3]);
 	}
 	rl_clear_history();
 	(void)argc;
