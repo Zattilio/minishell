2adc2f1b56b9c836961abca0c0d452e98fb94858
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2023-03-08 19:46:10 +0100

    modif qui sert a rien

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlanza <jlanza@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mbocquel <mbocquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 18:32:12 by jlanza            #+#    #+#             */
-/*   Updated: 2023/03/08 19:11:53 by jlanza           ###   ########.fr       */
+/*   Updated: 2023/03/08 19:45:54 by mbocquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 unsigned char	g_return_value = 0;
 
-/* ne plus trim les espaces */
 /*attention, ne pas utiliser l'enve du main*/
 /*
 int	main(int argc, char **argv, char **env)
